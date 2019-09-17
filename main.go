a34c3f734c66d85357e98507af511e2d0ff0e137
k725 <k725@users.noreply.github.com>
2019-09-17 19:35:30 +0900

    Add session sample

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"github.com/foolin/goview"
 	"github.com/foolin/goview/supports/echoview-v4"
 	"github.com/k725/go-simple-blog/config"
 	"github.com/k725/go-simple-blog/controller"
 	"github.com/k725/go-simple-blog/model"
+	"github.com/labstack/echo-contrib/session"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/srinathgs/mysqlstore"
 	"html/template"
 	"time"
 )
@@ -46,6 +49,16 @@ func main() {
 	//db.Create(&test)
 
 	e := echo.New()
+
+	con := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True", t.User, t.Password, t.Address, t.Database)
+	store, err := mysqlstore.NewMySQLStore(con, "session", "/", 3600, []byte("secret"))
+	if err != nil {
+		panic(err)
+	}
+
+	defer store.Close()
+
+	e.Use(session.Middleware(store))
 
 	e.Debug = true
 	e.Static("/", "public")
