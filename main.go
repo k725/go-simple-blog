53a8c1a9e4dcc11742c0300c803dde9da8e0ce02
k725 <k725@users.noreply.github.com>
2019-09-23 16:48:55 +0900

    Add error page #8

--- main.go
+++ main.go
@@ -60,6 +60,8 @@ func main() {
 	e.Debug = true
 	e.Static("/", "public")
 
+	e.HTTPErrorHandler = customHTTPErrorHandler
+
 	setupRender(e)
 	setupMiddleware(e)
 	setupRoute(e)
@@ -162,3 +164,26 @@ func ServerHeader(next echo.HandlerFunc) echo.HandlerFunc {
 		return err
 	}
 }
+
+func customHTTPErrorHandler(err error, c echo.Context) {
+	code := http.StatusInternalServerError
+	if he, ok := err.(*echo.HTTPError); ok {
+		code = he.Code
+	}
+	c.Logger().Error(err)
+
+	// Error page
+	errorPage := fmt.Sprintf("page/error/%d", code)
+	errorParam := map[string]interface{}{
+		"title": code,
+	}
+	if err := c.Render(code, errorPage, errorParam); err != nil {
+		c.Logger().Error(err)
+	}
+
+	// エラーページのテンプレートレンダリングで失敗したときの保険
+	if err := c.String(code, "Internal Server Error"); err != nil {
+		c.Logger().Error(err)
+	}
+}
+
