8b1370b6b8a5d5c5c5af9bdc1e66c1602a48e4ee
k725 <k725@users.noreply.github.com>
2019-10-24 00:50:31 +0900

    Add category flash message.

--- controller/admin/category.go
+++ controller/admin/category.go
@@ -1,9 +1,10 @@
 package admin
 
 import (
-	"errors"
+	"fmt"
 	"github.com/foolin/goview/supports/echoview-v4"
 	"github.com/k725/go-simple-blog/model"
+	"github.com/k725/go-simple-blog/service/sess"
 	"github.com/labstack/echo/v4"
 	"math"
 	"net/http"
@@ -13,9 +14,22 @@ import (
 
 func GetCategories(c echo.Context) error {
 	ca := model.GetAllCategories()
+
+	s, err := sess.GetSession(c)
+	if err != nil {
+		return err
+	}
+	eF := s.Flashes("error")
+	iF := s.Flashes("info")
+	if err := sess.SaveSession(c, map[string]interface{}{}); err != nil {
+		c.Error(err)
+	}
+
 	return echoview.Render(c, http.StatusOK, "page/admin/category", echo.Map{
 		"title":      "Categories",
 		"categories": ca,
+		"errorFlash": eF,
+		"infoFlash": iF,
 	})
 }
 
@@ -30,16 +44,25 @@ func PostCategory(c echo.Context) error {
 		if err := model.DeleteCategory(idi); err != nil {
 			if strings.HasPrefix(err.Error(), "Error 1451:") {
 				c.Logger().Info("Category has articles.")
+				if err := sess.SaveErrorFlash(c, "Category has articles."); err != nil {
+					c.Logger().Warn(err)
+				}
 				return c.Redirect(http.StatusFound, "/admin/category")
 			}
 			return err
+		}
+		if err := sess.SaveInfoFlash(c, "Success delete."); err != nil {
+			c.Logger().Warn(err)
 		}
 		return c.Redirect(http.StatusFound, "/admin/category")
 	}
 
 	cn := c.FormValue("name")
 	if len(cn) == 0 {
-		return errors.New("invalid category name")
+		if err := sess.SaveErrorFlash(c, "Invalid category name"); err != nil {
+			c.Logger().Warn(err)
+		}
+		return c.Redirect(http.StatusFound, "/admin/category")
 	}
 
 	ca := model.Category{
@@ -48,7 +71,9 @@ func PostCategory(c echo.Context) error {
 	if err := model.InsertCategory(ca); err != nil {
 		return err
 	}
-
+	if err := sess.SaveInfoFlash(c, fmt.Sprintf("Added category '%s'", cn)); err != nil {
+		c.Logger().Warn(err)
+	}
 	return c.Redirect(http.StatusFound, "/admin/category")
 }
 
