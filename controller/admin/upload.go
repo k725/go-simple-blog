77fdfa71795bc673d02a37700d2d7ca6e776d4cf
k725 <k725@users.noreply.github.com>
2019-11-29 21:10:08 +0900

    Add image upload.

--- controller/admin/upload.go
+++ controller/admin/upload.go
@@ -2,11 +2,38 @@ package admin
 
 import (
 	"github.com/labstack/echo/v4"
+	"io"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 func PostUploadFile(c echo.Context) error {
+	file, err := c.FormFile("image")
+	if err != nil {
+		return err
+	}
+	src, err := file.Open()
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	dstPath := filepath.Join("public", "image", "article", file.Filename) // @todo fix
+	dst, err := os.Create(dstPath)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	if _, err = io.Copy(dst, src); err != nil {
+		return err
+	}
+
 	return c.JSON(http.StatusOK, &echo.Map{
 		"status": true,
+		"data": echo.Map{
+			"filePath":	"image/article/" + file.Filename,
+		},
 	})
 }
